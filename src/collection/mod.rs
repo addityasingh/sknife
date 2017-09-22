6d721aad0e08d3d47b62f8d73be8924664fd8061
Aditya Pratap Singh <adisinghrajput@gmail.com>
2017-09-22 12:53:38 +0200

    Add reduce function

--- src/collection/mod.rs
+++ src/collection/mod.rs
@@ -3,10 +3,12 @@ pub use self::any::any;
 pub use self::count::count;
 pub use self::find::find;
 pub use self::map::map;
+pub use self::reduce::reduce;
 pub use self::zip::zip;
 
 mod any;
 mod count;
 mod find;
 mod map;
+mod reduce;
 mod zip;
---
+++ src/collection/reduce.rs
@@ -0,0 +1,68 @@
+
+#![crate_name = "sknife"]
+
+/// Reduce a vector/list using an accumulating function
+///
+/// # Arguments
+///
+/// * `vect` - A vector to apply the map to
+/// * `reduce_fn` - Accumulating function to apply to the list
+/// * `initial` - An initial value to accumulate upon
+///
+/// # Example
+///
+/// ```
+/// use sknife::collection::reduce;
+/// let initial = 1;
+/// let list = vec![1, 2, 3];
+/// let fact = |acc, x| acc * x;
+/// reduce(fact, list, initial);
+/// 
+/// ```
+/// 
+/// # Result
+/// ```
+/// 6;
+/// ```
+pub fn reduce<F, A> (mut reduce_fn: F, vect: Vec<A>, initial: A) -> A 
+    where F: FnMut(A, A) -> A,
+    A: Clone {
+    let mut accum: A = initial;
+    let mut iterator = vect.iter();
+
+    loop {
+        match iterator.next() {
+            Some(v) => { 
+                accum = reduce_fn(accum, v.clone()); 
+            },
+            None => { break; }
+        }
+    }
+
+    accum
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn reduce_fact() {
+        let list = vec![1, 2, 3];
+        let fact = |acc: i32, x| acc * x;
+        assert_eq!(
+            reduce(fact, list, 1), 
+            6
+        );
+    }
+
+    #[test]
+    fn reduce_sum() {
+        let list = vec![1, 2, 3];
+        let sum = |acc, x| acc + x;
+        assert_eq!(
+            reduce(sum, list, 0), 
+            6
+        );
+    }
+}
